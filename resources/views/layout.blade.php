c0ea6e777702684eef602a6808146ffef461448c
Neochees <113420296+Neochees@users.noreply.github.com>
2024-08-29 16:56:44 +0200

    les 15 begin WIP

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<link rel="stylesheet" href="/app.css">
+<head>
+    <title>My blog</title>
+</head>
+<body>
+
+@yield('content')
+
+</body>
+</html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,40 @@
-<!doctype html>
-<html lang="en">
-<link rel="stylesheet" href="/app.css">
-<head>
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
 
-<article>
-@foreach($posts as $post)
-    <h1><a href="/posts/<?= $post->slug; ?>">
-        {!! $post->title !!}
-        </a>
-    </h1>
+@section('çontent')
+    @foreach($posts as $post)
+        <article>
 
-    <div>{!! $post->excerpt !!}</div>
-@endforeach
-</article>
+            <h1><a href="/posts/<?= $post->slug; ?>">
+                    {!! $post->title !!}
+                </a>
+            </h1>
+
+            <div>{!! $post->excerpt !!}</div>
+
+        </article>
+    @endforeach
+@endsection
 
 
-</body>
-</html>
+{{--<!doctype html>--}}
+{{--<html lang="en">--}}
+{{--<link rel="stylesheet" href="/app.css">--}}
+{{--<head>--}}
+{{--    <title>My blog</title>--}}
+{{--</head>--}}
+{{--<body>--}}
+{{--@foreach($posts as $post)--}}
+{{--<article>--}}
+
+{{--    <h1><a href="/posts/<?= $post->slug; ?>">--}}
+{{--        {!! $post->title !!}--}}
+{{--        </a>--}}
+{{--    </h1>--}}
+
+{{--    <div>{!! $post->excerpt !!}</div>--}}
+
+{{--</article>--}}
+{{--@endforeach--}}
+
+{{--</body>--}}
+{{--</html>--}}
