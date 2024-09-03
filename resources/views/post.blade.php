954060206a6cd9ba93c6941d443b0e03596eeedd
Neochees <113420296+Neochees@users.noreply.github.com>
2024-09-03 10:16:52 +0200

    les 24 af

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,9 +2,14 @@
     <article>
         <h1>{!! $post->title !!}</h1>
 
+        <p>
+            <a href="#">{{ $post->category->name }}</a>
+        </p>
+
         <div>
             {!! $post->body !!}
         </div>
+        <a href="/">Back</a>
     </article>
 </x-layout>
 
