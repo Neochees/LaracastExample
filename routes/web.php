e96eb1aea8a490f033d729b0d0ebc5f085f56484
Neochees <113420296+Neochees@users.noreply.github.com>
2024-09-05 15:36:52 +0200

    les 35 af

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
         'categories' => Category::all()
     ]);
 
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
@@ -26,7 +26,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('author/{author:username}', function (\App\Models\User $author) {
     return view('posts', [
