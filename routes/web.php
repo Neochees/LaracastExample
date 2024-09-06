fe5e91e93d68da01d18360d84ae35421ce506ccb
Neochees <113420296+Neochees@users.noreply.github.com>
2024-09-06 10:38:17 +0200

    les 38 af begin aan filter hooftstuk

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,15 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use function PHPSTORM_META\map;
 
-Route::get('/', function () {
-    $posts = Post::latest();
+Route::get('/', [\App\Http\Controllers\PostController::class, 'index'])->name('home');
 
-    if (request('search')) {
-        $posts
-            ->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-    }
-    return view('posts', [
-        'posts' => $posts->get(),
-        'categories' => Category::all()
-    ]);
-
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
