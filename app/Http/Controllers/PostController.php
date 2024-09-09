427666dd5931d6bc7e0e2eab940d5179ae297bc9
Neochees <113420296+Neochees@users.noreply.github.com>
2024-09-09 12:30:42 +0200

    les 39 af

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,8 +13,9 @@ class PostController extends Controller
     {
 
         return view('posts', [
-            'posts' => Post::latest()->filter(request(['search']))->get(),
+            'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
             'categories' => Category::all(),
+            'currentCategory' => Category::firstWhere('slug', request('category'))
         ]);
     }
     public function show(Post $post)
